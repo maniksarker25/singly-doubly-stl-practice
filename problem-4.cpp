959d83a789ab444ff67ededb9cf4736a3aad0627
manik <maniksarker652@gmail.com>
2025-01-03 12:07:31 +0600

    make function for insert at and and insert at any position

--- problem-4.cpp
+++ problem-4.cpp
@@ -52,25 +52,50 @@ void insert_at_tail(Node *&head, Node *&tail, int val)
     tail = newNode;
 }
 
+void insert_at_any_post(Node *head, int idx, int val)
+{
+
+    Node *newNode = new Node(val);
+
+    Node *temp = head;
+    for (int i = 1; i < idx; i++)
+    {
+        temp = temp->next;
+    }
+
+    newNode->next = temp->next;
+    temp->next->prev = newNode;
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+void insert_at_head(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+}
+
 int main()
 {
     Node *head = NULL;
     Node *tail = NULL;
 
-    int val;
-    while (1)
-    {
-        cin >> val;
-        if (val == -1)
-        {
-            break;
-        }
-        insert_at_tail(head, tail, val);
+    int q;
+    cin >> q;
+    while(q--){
+        int x,v;
+        cin >> x >> v;
+
     }
- 
-    print_left_to_right(head);
-    cout << endl;
-    print_right_to_left(tail);
 
     return 0;
 }
