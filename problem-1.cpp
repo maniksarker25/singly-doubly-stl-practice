2988407b75b26dc4ee9a4a765f92953d4a5ddfd9
manik <maniksarker652@gmail.com>
2025-01-03 10:46:43 +0600

    solve first problem

---
+++ problem-1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{ 
+   list<int>list;
+   int val;
+   while(true){
+    cin >> val;
+    if(val == -1){
+        break;
+    }
+    list.push_back(val);
+   }
+   list.sort();
+   list.unique();
+
+   for(int val : list){
+    cout << val << " ";
+   }
+    return 0; 
+} 
