1d92276b6767324a8ac45c75a548d35313860391
manik <maniksarker652@gmail.com>
2025-01-03 12:45:50 +0600

    solve 5th problem with stl list

---
+++ problem-5.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    list<int> list;
+    int q;
+    cin >> q;
+
+    while (q--) {
+        int x, v;
+        cin >> x >> v;
+
+        if (x == 0) {
+            list.push_front(v);
+        } 
+        else if (x == 1) {
+            list.push_back(v);
+        } 
+        else if (x == 2) {
+            if (v >= 0 && v < list.size()) {
+                auto it = list.begin();
+                for (int i = 0; i < v; i++) {
+                    ++it;
+                }
+                list.erase(it);
+            }
+        }
+
+        cout << "l -> ";
+        for (auto it = list.begin(); it != list.end(); ++it) {
+            cout << *it << " ";
+        }
+        cout << endl;
+
+        cout << "r -> ";
+        for (auto it = list.rbegin(); it != list.rend(); ++it) {
+            cout << *it << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
